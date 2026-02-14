c9a5ce506351905f0b1701cf0f62a423b2ed8d31
shadkhan8815 <shadkhan8516@gmail.com>
2026-02-14 15:07:52 +0530

    done

---
+++ Project-10/src/main/java/com/rays/ORSProject10Application.java
@@ -0,0 +1,14 @@
+package com.rays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ORSProject10Application {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ORSProject10Application.class, args);
+	}
+
+}
